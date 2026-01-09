d1cfd05df853a41011a9eed0d632355aa52eb55e
Tharaka Sandaruwan <tharaka.mts@gmail.com>
2026-01-09 23:46:01 +0530

    refactor(web): import navbar & update component  to an arrow function

--- apps/web/src/app/(dashboard)/dashboard/page.tsx
+++ apps/web/src/app/(dashboard)/dashboard/page.tsx
@@ -1,8 +1,13 @@
-export default function DashboardPage() {
+import NavBar from "@/components/ui/NavBar";
+
+const DashboardPage = () => {
     return (
         <div>
+            <NavBar />
             <h2 className="text-2xl font-bold mb-4">Dashboard</h2>
             <p className="text-muted-foreground">Welcome to your dashboard.</p>
         </div>
-    );
+    )
 }
+
+export default DashboardPage;
